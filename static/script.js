6084cd3bc7fced77a39b0dc9c716d7a65b6a8d4b
Andrea Coppola <coppola.andrea@iisgalvanimi.edu.it>
2026-04-23 10:58:51 +0000

    manca il login con post

--- static/script.js
+++ static/script.js
@@ -1,4 +1,5 @@
 async function controllaCredenziali() {
+    
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
     if (!username || !password)
@@ -7,9 +8,28 @@ async function controllaCredenziali() {
     const dati = await res.json();
     document.getElementById('Risultato').innerText = dati.messaggio;
 } 
+
 document.getElementById('btn_registrati').addEventListener('click',controllaCredenziali)
 if (dati.messaggio === 0  ) {
     document.getElementById('Risultato').innerText = "accesso negato , username o password errati ";
     document.getElementById('username').value = "";
     document.getElementById('password').value = "";
 }
+
+    const res = await fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: `username=${username}&password=${password}`
+    });
+     const json = await res.json();
+
+    if (json.messaggio == 1){
+        document.getElementById("risultato").innerText = "Accesso effettuato";
+    }
+    else {
+        document.getElementById("risultato").innerText = "Accesso negato";
+    }
+
+document.getElementById('bottone').addEventListener('click', controllaCredenziali);
